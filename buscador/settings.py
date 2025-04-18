17d6d89a1785fbbdd20801803e17dbb6122482b1
oswaldo-herrera <oswaldo.herrera.mx@gmail.com>
2025-04-18 12:09:04 -0600

    se integro la configuracion para manejar los archivos media y static tanto la configuarcion de allohost para poder recibir cualquier dominio al momento de hacer deploy y por ultimo se agego el requirements

--- buscador/settings.py
+++ buscador/settings.py
@@ -29,7 +29,7 @@ YOUTUBE_API_KEY = os.getenv("API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
